f9299f1c00f1a2e87897d59dd3ebcc77eb80cc89
LeeKangHyun-lab <ka09068@gmail.com>
2025-02-24 15:19:15 +0900

    [level 0] Title: 몫 구하기, Time: 0.04 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public static int solution(int num1, int num2) {
+        int answer = -1;
+        answer = num1 / num2;
+        return answer;
+    }
+    
+    public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    
+    int num1 = scan.nextInt();
+    int num2 = scan.nextInt();
+    solution(num1,num2);
+        
+    }
+}
