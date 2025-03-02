0de2efcc2de8cfd47cda1f46cabc018760978b07
LeeKangHyun-lab <ka09068@gmail.com>
2025-03-02 18:15:49 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 84.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Solution {
+    public static int solution(int angle) {
+        int answer = 0;
+        
+        if(0 < angle && angle < 90)
+            answer = 1;
+        else if(angle == 90)
+            answer = 2;
+        else if(90 < angle && angle < 180)
+            answer = 3;
+        else if(angle == 180)
+            answer = 4;
+        
+        return answer;
+    }
+    
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        
+        int angle = scan.nextInt();
+        
+        solution(angle);
+    }
+}
