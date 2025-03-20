f2d97bd087cc80a23ceda94ed0666e456dd19aee
LeeKangHyun_lab <ka09068@gmail.com>
2025-03-20 18:54:02 +0900

    [Bronze I] Title: 약수들의 합, Time: 200 ms, Memory: 17908 KB -BaekjoonHub

---
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+	 public static void main(String[] args){
+	    	Scanner scan = new Scanner(System.in);
+	    	
+	    	while(true) {
+	    		int n = scan.nextInt();
+	    		if(n == -1) break;
+	    		ArrayList<Integer> list = new ArrayList<>();
+	    		int sum = 0;
+	    		for (int i = 1; i < n ; i++) {
+					if(n % i == 0) {
+						list.add(i);
+						sum += i;
+					}
+				}
+	    		if(n == sum) {
+	    			System.out.print(n + " = ");
+	    			for(int i = 0; i < list.size(); i++) {
+	    				System.out.print(list.get(i));
+		    			if(i < list.size() -1) {
+		    				System.out.print(" + ");
+		    		}
+	    		}
+	    			System.out.println();
+	    	}
+	    		else {
+	    			System.out.println(n + " is NOT perfect.");
+	    		}
+	    }
+	    	scan.close();
+    }
+}
